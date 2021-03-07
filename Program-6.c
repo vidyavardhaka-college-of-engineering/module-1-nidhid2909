e78989b30d9a9858ed4e218f6dc77581fb78bb40
nidhid2909 <nidhid3260@gmail.com>
2021-03-07 08:19:37 +0000

    done

--- Program-6.c
+++ Program-6.c
@@ -7,7 +7,7 @@ int main()
 int a,b,result;
 printf("Enter 2 numbers for Arithmetic evaluation operation:");
 scanf("%d%d",&a,&b);  // correct the code
-result = a+b*a/b-a%b;      // correct the code
+result =(a+b*a/b-a%b) ;      // correct the code
 printf("Arithmetic expression evaluation of %d and  %d is = %d",a,b,result); // correct the code
 return 0;// insert the missing code
 }
